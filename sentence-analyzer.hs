fcd40a2d68ec70d9a7a4b32434182236c2b0f4ef
Nicholas Balarezo <143428049+ngbalarezo@users.noreply.github.com>
2025-04-29 12:59:25 -0400

    Implemented word count and character counts.

--- sentence-analyzer.hs
+++ sentence-analyzer.hs
@@ -1,2 +1,21 @@
+import Data.List (group, sort)
+
+wordCountTotal :: String -> Int
+wordCountTotal s = length $ words s
+
+wordCountEach :: String -> [(String, Int)]
+wordCountEach s = map (\w -> (head w, length w)) $ group $ sort $ words s
+
+characterCount :: String -> Int
+characterCount s = length s
+
+-- MAIN
 main :: IO()
-main = putStrLn "Hello World."
+main = do
+  putStrLn "Sentence Analyzer: "
+  putStr "  Total Word Count: "
+  print(wordCountTotal "The dog is running from the police. The dog is fast, but the police are faster.")
+  putStr "  Word Count per Word: "
+  print(wordCountEach "The dog is running from the police. The dog is fast, but the police are faster.")
+  putStr "  Total Character Count: "
+  print(characterCount "The dog is running from the police. The dog is fast, but the police are faster.")

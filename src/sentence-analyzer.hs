3688ff9abb9d256c723718d09fe232f75eb15549
Nicholas Balarezo <143428049+ngbalarezo@users.noreply.github.com>
2025-05-03 22:33:40 -0400

    Added documentation and rewrote the final input sentence.

--- src/sentence-analyzer.hs
+++ src/sentence-analyzer.hs
@@ -3,20 +3,26 @@ import Data.List (group, sort, sortBy)
 import Data.Ord (comparing)
 import Text.Regex (mkRegex, splitRegex)
 
+-- Function counts number of sentences, not counting duplicate punctuation as a sentence
 sentenceCount :: String -> Int
 sentenceCount s = length 
                 $ filter (not . null)
                 $ splitRegex (mkRegex "[.!?]+[\\s]*") s
 
+-- Function counts number of words by converting the sentence to an array of words and returning the length
 wordCount :: String -> Int
 wordCount s = length $ words s
 
+-- Function removes the punctuation from a sentence, used in other functions throughout the program
 removePunctuation :: String -> String
 removePunctuation s = map (\c -> if isAlphaNum c then toLower c else ' ') s 
 
+-- Function converts word frequency tuples by removing the parentheses, returns an array of formatted tuples
 formatTuples :: [(String, Int)] -> [String]
 formatTuples a = map (\(w, c) -> "       " ++ w ++ ", " ++ show c) a
 
+-- Functions converts sentence into an array of formatted tuples, each with the word and frequency of the word.
+-- The array returned is sorted from most common to least common words.
 wordFrequency :: String -> [String]
 wordFrequency s = formatTuples 
                 $ sortBy (flip (comparing snd)) 
@@ -26,20 +32,22 @@ wordFrequency s = formatTuples
                 $ words
                 $ removePunctuation s
 
+-- Function recursively cuts off the front and back of the string until the middle word or two words are left.
 middleWord :: String -> String
 middleWord s | length (words s) == 1 = head (words s)
              | length (words s) == 2 = s
              | otherwise = middleWord $ unwords $ tail $ init $ words $ removePunctuation s
 
+-- Functions returns the number of characters in the sentence including whitespace characters
 characterCount :: String -> Int
 characterCount s = length s
 
--- MAIN 
+-- main
 main :: IO()
 main = do
-  putStrLn "=================="
-  putStrLn "Sentence Analyzer:"
-  putStrLn "==================\n"
+  putStrLn "=========================="
+  putStrLn "Haskell Sentence Analyzer:"
+  putStrLn "==========================\n"
   putStr "Sentence(s): "
   print (sentence)
   putStrLn ""
@@ -55,5 +63,6 @@ main = do
   print (characterCount sentence)
   putStrLn ""
 
+-- Input string
 sentence :: String
-sentence = "The dog is running from the police. The dog is fast but the police are faster."
+sentence = "The dog is running from the police. The dog is fast, but the police are faster! The dog panics and screeches."
